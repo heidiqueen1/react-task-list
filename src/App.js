55748cdf40cc58653dc73fb74722ecdea37338c3
Heidi Sánchez <heidisanchez103.9@gmail.com>
2022-06-07 18:19:22 -0500

    se agregó el Router a la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
-import Logo from './componentes/Logo.jsx';
-import EstatusTareas from './componentes/EstatusTareas.jsx';
-
-
-
+import "./App.css";
+import Logo from "./componentes/Logo.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import Profile from "./Pages/Profile.jsx";
+import Task from "./Pages/Task.jsx";
 
 function App() {
   return (
-    <div className='aplicacion-tareas'>
-      <Logo />
-    <div className='tareas-lista-principal'>
-     
-     <div>
-     <EstatusTareas 
-      estatus = 'To Do'
-      />
-     </div>
-     </div>
-     </div>      
-    
+    <Router>
+      <div className="aplicacion-tareas">
+        <Logo />
+        <div className="tareas-lista-principal">
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/task" element={<Task status= "To Do" />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Pages/Task.jsx
+++ src/Pages/Task.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../App.css';
-import ListaDeTareas from './ListaDeTareas';
+import ListaDeTareas from '../componentes/ListaDeTareas';
 
 
-function EstatusTareas({estatus}){
+function Task({estatus}){
     return(
         <div>
         <h2> Status {estatus}/ <span className='palabra-primera'>In Process/ </span><span className='palabra-segunda'>Done</span></h2>
@@ -16,4 +16,4 @@ function EstatusTareas({estatus}){
     )
 }
 
-export default EstatusTareas;
+export default Task;

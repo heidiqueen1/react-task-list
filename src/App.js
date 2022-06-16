568ef8082b0955c2ec0a110d205d1d259d96f841
Heidi Sánchez <heidisanchez103.9@gmail.com>
2022-06-15 19:35:16 -0500

    agregué elementos de chakra UI

--- src/App.js
+++ src/App.js
@@ -4,28 +4,54 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Profile from "./Pages/Profile.jsx";
 import Task from "./Pages/Task.jsx";
+import { Box, Button, Stack } from "@chakra-ui/react";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <button className='tarea-boton-nav'><Link className="link" to='/'> Home </Link></button>
-        <button className='tarea-boton-nav'><Link className="link" to='/profile'> Profile </Link></button>
-        <button className='tarea-boton-nav'><Link className="link" to='/task'> Task </Link></button>
-      </nav>
-      <div className="aplicacion-tareas">
-        <Logo />
-        <div className="tareas-lista-principal">
-          <div>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/task" element={<Task />} />
-            </Routes>
+      <Box
+        color="#ffff"
+        bg="#8FD8D2"
+        fontFamily="'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
+        pt="20px"
+        minH="100vh"
+      >
+        <nav>
+        <Stack direction='row' spacing={40} align='center' justifyContent='center' m='40px'>
+          <Button  size="lg" color="bisque" variant='outline'>
+            <Link className="link" to="/">
+              {" "}
+              Home{" "}
+            </Link>
+          </Button>
+          <Button  size="lg" color="bisque" variant='outline'>
+            <Link className="link" to="/profile">
+              {" "}
+              Profile{" "}
+            </Link>
+          </Button>
+          <Button  size="lg" color="bisque" variant='outline' >
+            <Link className="link" to="/task">
+              {" "}
+              Task{" "}
+            </Link>
+          </Button>
+          </Stack>
+        </nav>
+        <div className="aplicacion-tareas">
+          <Logo />
+          <div className="tareas-lista-principal">
+            <div>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/task" element={<Task />} />
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
+      </Box>
     </Router>
   );
 }
